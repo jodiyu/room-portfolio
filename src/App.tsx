b86e683de3564a940038ad1719412dc2ec478c82
jodiyu <jodiyu@bu.edu>
2026-03-08 12:13:56 -0700

    Basic room layout with blocks for spatial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { Canvas } from '@react-three/fiber'
+import RoomScene from './scene/RoomScene'
 
 export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[2, 2, 3]} intensity={2} />
-
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="hotpink" />
-        </mesh>
+      <Canvas camera={{ position: [0, 1.6, 5], fov: 60 }}>
+        <RoomScene />
       </Canvas>
     </div>
   )
---
+++ src/scene/RoomScene.tsx
@@ -0,0 +1,111 @@
+export default function RoomScene() {
+  return (
+    <>
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[2, 4, 3]} intensity={2} />
+
+      {/* Floor */}
+      <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[12, 12]} />
+        <meshStandardMaterial color="#7b5e57" />
+      </mesh>
+
+      {/* Ceiling */}
+      <mesh position={[0, 3, 0]} rotation={[Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[12, 12]} />
+        <meshStandardMaterial color="#f1f1eb" />
+      </mesh>
+
+      {/* Back wall */}
+      <mesh position={[0, 1, -6]}>
+        <planeGeometry args={[12, 8]} />
+        <meshStandardMaterial color="#d9d4cc" />
+      </mesh>
+
+      {/* Left wall */}
+      <mesh position={[-6, 1, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <planeGeometry args={[12, 8]} />
+        <meshStandardMaterial color="#d1cdc5" />
+      </mesh>
+
+      {/* Right wall */}
+      <mesh position={[6, 1, 0]} rotation={[0, -Math.PI / 2, 0]}>
+        <planeGeometry args={[12, 8]} />
+        <meshStandardMaterial color="#d1cdc5" />
+      </mesh>
+
+      {/* Front wall */}
+      <mesh position={[0, 1, 6]} rotation={[0, Math.PI, 0]}>
+        <planeGeometry args={[12, 8]} />
+        <meshStandardMaterial color="#d9d4cc" />
+      </mesh>
+
+      {/* Desk */}
+      <mesh position={[0, -0.2, -3.8]}>
+        <boxGeometry args={[2.8, 1.2, 1.2]} />
+        <meshStandardMaterial color="#6b4f3a" />
+      </mesh>
+
+      {/* Computer monitor on desk */}
+      <mesh position={[0, 0.8, -3.4]}>
+        <boxGeometry args={[1.2, 0.8, 0.1]} />
+        <meshStandardMaterial color="#222222" />
+      </mesh>
+
+      {/* Bookshelf */}
+      <mesh position={[-5.3, 0.2, 0]}>
+        <boxGeometry args={[0.9, 2.8, 1.1]} />
+        <meshStandardMaterial color="#8a6742" />
+      </mesh>
+
+      {/* Left wall window */}
+      <mesh position={[-5.95, 1, -3]} rotation={[0, Math.PI / 2, 0]}>
+        <planeGeometry args={[3, 2]} />
+        <meshStandardMaterial color="#87ceeb" />
+      </mesh>
+
+      {/* Left wall window */}
+      <mesh position={[-5.95, 1, 3]} rotation={[0, Math.PI / 2, 0]}>
+        <planeGeometry args={[3, 2]} />
+        <meshStandardMaterial color="#87ceeb" />
+      </mesh>
+
+      {/* Bed */}
+      <mesh position={[4.9, -0.7, -4]}>
+        <boxGeometry args={[2.2, 0.6, 6.5]} />
+        <meshStandardMaterial color="#8a6742" />
+      </mesh>
+
+      {/* Diploma */}
+      <mesh position={[3.2, 1.5, -5.95]}>
+        <boxGeometry args={[1.4, 1, 0.05]} />
+        <meshStandardMaterial color="#f5f0d8" />
+      </mesh>
+
+      {/* Dance poster */}
+      <mesh position={[-2.4, 1.4, -5.95]}>
+        <boxGeometry args={[1.4, 2, 0.05]} />
+        <meshStandardMaterial color="#b3476b" />
+      </mesh>
+
+      {/* Clock */}
+      <mesh position={[5.95, 1.5, 0]} rotation={[0, -Math.PI / 2, 0]}>
+        <circleGeometry args={[0.6, 32]} />
+        <meshStandardMaterial color="#f5f0d8" />
+      </mesh>
+
+      {/* Dance bag */}
+      <mesh position={[-5.2, -0.55, -5.2]}>
+        <boxGeometry args={[0.8, 0.9, 0.5]} />
+        <meshStandardMaterial color="#4a3060" />
+      </mesh>
+
+      {/* Door */}
+      <mesh position={[0, 0, 5.95]} rotation={[0, Math.PI, 0]}>
+        <planeGeometry args={[1.8, 3.6]} />
+        <meshStandardMaterial color="#5a3a28" />
+      </mesh>
+
+    </>
+  )
+}
